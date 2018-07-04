33dfaec54aab7a2e85e5df56439b7123a9ca2e5f
unknown <melina.hartley@redlab.io>
2018-07-04 16:39:20 +0200

    DOne

---
+++ Tables/Dimension/Vendor.sql
@@ -0,0 +1,15 @@
+CREATE TABLE dim.Vendor
+{
+    [MVendor_ID]  bigint IDENTITY(1,1) NOT NULL,
+    [Vendor Name] NVARCHAR (20) NOT NULL
+    [Vendor Code] NVARCHAR (20) NOT NUlL,
+    [Vendor Description] NVARCHAR (20) NOT NULL,
+    [Vendor Type] NVARCHAR (20) NOT NULL,
+    [Vendor Description] NVARCHAR (20) NOT NULL,
+    [Material Location] NVARCHAR (20) NOT NULL,
+    [EFF_DATE] NVARCHAR (20) NOT NULL,
+    [END_DATE] NVARCHAR (20) NOT NULL,
+    [IS_CURRENT] NVARCHAR (20) NOT NULL
+
+    
+}
