40a2f722b861fcb76889d89d54d5c96b0b2bb9f3
unknown <melina.hartley@redlab.io>
2018-07-06 09:27:23 +0200

    end

--- Star/Tables/Fact/Summary_Fact.sql
+++ Star/Tables/Fact/Summary_Fact.sql
@@ -1,20 +1,18 @@
-DROP TABLE IF EXISTS [fact].[Stamping];
+DROP TABLE IF EXISTS [fact].[Summary];
 
-CREATE TABLE [fact].[Stamping]
+CREATE TABLE [fact].[Summary]
 (
-	[STAMPING_KEY]	                                	BIGINT IDENTITY(1,1)     NOT NULL,
-    [Process Order ID]                                  NVARCHAR (50)            NOT NULL,
+	[SUMMARY_KEY]	                                	BIGINT IDENTITY(1,1)     NOT NULL,
 	[Process Date start]	                            DATETIMEOFFSET(7)        NOT NULL,
-	[Process Date End]	    	                        DATETIMEOFFSET(7)        NOT NULL,
-    [Process Hour Duration]                             DECIMAL(19,2)            NOT NULL,
-    [Equipment Set up Duration]                         DECIMAL(19,2)            NOT NULL,
+    [Total Process Hour Duration]                       DECIMAL(19,2)            NOT NULL,
+    [Total Equipment Set up Duration]                   DECIMAL(19,2)            NOT NULL,
     [Total Scheduled Hour Downtime]                     DECIMAL(19,2)            NOT NULL,
-    [Machine Malfuction Hour Duration]                  DECIMAL(19,2)            NOT NULL,
+    [Total Machine Malfuction Hour Duration]            DECIMAL(19,2)            NOT NULL,
     [Total Unscheduled Downtime]                        DECIMAL(19,2)            NOT NULL,
     [Scraps Measurement K]                              DECIMAL(19,2)            NOT NULL,
-    [Units of Scraps]                                   DECIMAL(19,2)            NOT NULL,
-    [Finished Good Measurement K]                       DECIMAL(19,2)            NOT NULL,   
-    [Finished Units of Good]                            DECIMAL(19,2)            NOT NULL,
-    [Reworked Scraps Measurement K]                     DECIMAL(19,2)            NOT NULL,
-    [Units of Reworked Scraps]                          DECIMAL(19,2)            NOT NULL,
+    [Total Units of Scraps]                             DECIMAL(19,2)            NOT NULL,
+    [Total Finished Good Measurement K]                 DECIMAL(19,2)            NOT NULL,   
+    [Total Finished Units of Good]                      DECIMAL(19,2)            NOT NULL,
+    [Total Reworked Scraps Measurement K]               DECIMAL(19,2)            NOT NULL,
+    [Total Units of Reworked Scraps]                    DECIMAL(19,2)            NOT NULL,
 );
