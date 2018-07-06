1de1d30858982d29dce1e886ddbf0c62c4edf554
unknown <melina.hartley@redlab.io>
2018-07-06 12:09:54 +0200

    DONE

---
+++ SnowFlake/Star/Reason.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS [dim].[Reason];
+
+CREATE TABLE [dim].[Reason]
+(
+    [Reason_KEY]			BIGINT IDENTITY(1,1)	NOT NULL,
+    [Reason Code]			NVARCHAR(50)			NOT NULL,
+	[Reason Name]			NVARCHAR(50)			NOT NULL,
+    [Reason Type]           NVARCHAR(50)            NOT NULL,
+    [Reason Description]	NVARCHAR(50)			NOT NULL,
+    [SMED_FLAG]             BIT                     NOT NULL,
+	[EFF_DATE]				DATETIMEOFFSET(7)		NOT NULL,
+	[END_DATE]				DATETIMEOFFSET(7)		NOT NULL,
+	[IS_CURRENT]			BIT						NOT NULL  
+);
