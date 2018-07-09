56f4a48158f457011fd4b1f48d2a4bb1d9f66969
unknown <melina.hartley@redlab.io>
2018-07-09 13:40:51 +0200

    calender

--- Star/Tables/Dimension/Calender.sql
+++ Star/Tables/Dimension/Calender.sql
@@ -3,5 +3,13 @@ DROP TABLE IF EXISTS [dim].[Calender];
 CREATE TABLE [dim].[Calender]
 (
 	[CALENDER_KEY]							BIGINT IDENTITY(1,1)	NOT NULL,
-	[]							
+	[Process Date Start]                    DATETIMEOFFSET(7)       NOT NULL,
+    [Date Euro]                             DATE(7)                 NOT NULL,
+    [Date US]	                            DATE(7)                 NOT NULL,
+    [Day of the Year]                       NVARCHAR(7)             NOT NULL,
+    [Week of the Year]                      NVARCHAR(7)             NOT NULL,
+    [Month of the Year]                     NVARCHAR(7)             NOT NULL,
+    [Quarter]                               NVARCHAR(7)             NOT NULL,
+    [Trimester]                             NVARCHAR(7)             NOT NULL,
+    [Year]						            NVARCHAR(7)             NOT NULL
 );
