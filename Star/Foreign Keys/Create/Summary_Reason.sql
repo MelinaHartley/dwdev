49c05b73f1aa8c4c76dcca497fa72b55ca1c2aa0
unknown <melina.hartley@redlab.io>
2018-07-09 14:21:24 +0200

    cleanup

--- Star/Foreign Keys/Create/Summary_Reason.sql
+++ Star/Foreign Keys/Create/Summary_Reason.sql
@@ -1,3 +1,3 @@
-LTER TABLE [fact].[Summary]     
+ALTER TABLE [fact].[Summary]     
 ADD CONSTRAINT FK_Summary_Reason_REASON_KEY FOREIGN KEY (REASON_KEY)     
     REFERENCES [dim].[Reason] (REASON_KEY); 
