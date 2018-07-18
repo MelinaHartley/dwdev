871fd9aab6a4a01fa8f078eeeafdde06fffba7f7
unknown <melina.hartley@redlab.io>
2018-07-18 11:13:33 +0200

    gg

--- Star/Foreign Keys/Create/Mixing_Production_Measure.sql
+++ Star/Foreign Keys/Create/Mixing_Production_Measure.sql
@@ -1,3 +1,3 @@
 ALTER TABLE [fact].[Mixing]      
 ADD CONSTRAINT FK_Mixing_Production_Measure_PRODUCTION_MEASURE_KEY FOREIGN KEY (PRODUCTION_MEASURE_KEY)     
-    REFERENCES [Utility].[Production_Measure] (PRODUCTION_MEASURE_KEY); 
+    REFERENCES [Utility].[Production Measure] (PRODUCTION_MEASURE_KEY); 
--- Star/Foreign Keys/Create/Summary_Production_Measure.sql
+++ Star/Foreign Keys/Create/Summary_Production_Measure.sql
@@ -1,3 +1,3 @@
 ALTER TABLE [fact].[Summary]      
 ADD CONSTRAINT FK_Summary_Production_Measure_PRODUCTION_MEASURE_KEY FOREIGN KEY (PRODUCTION_MEASURE_KEY)     
-    REFERENCES [Utility].[Production_Measure] (PRODUCTION_MEASURE_KEY); 
+    REFERENCES [Utility].[Production Measure] (PRODUCTION_MEASURE_KEY); 
