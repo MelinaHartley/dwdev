151ce192b2297828896b59780e39f3f37b42373f
unknown <melina.hartley@redlab.io>
2018-07-18 10:58:13 +0200

    change FK

--- Star/Foreign Keys/Create/Curing_Calender.sql
+++ Star/Foreign Keys/Create/Curing_Calender.sql
@@ -1,3 +1,3 @@
 ALTER TABLE [fact].[Curing]      
 ADD CONSTRAINT FK_Curing_Calender_CALENDER_KEY FOREIGN KEY (CALENDER_KEY)     
-    REFERENCES [Utility].[Calender] (CALENDER_KEY); 
+    REFERENCES [dim].[Calender] (CALENDER_KEY); 
--- Star/Foreign Keys/Create/Summary_Calender.sql
+++ Star/Foreign Keys/Create/Summary_Calender.sql
@@ -1,3 +1,3 @@
 ALTER TABLE [fact].[Summary]      
 ADD CONSTRAINT FK_Summary_Calender_CALENDER_KEY FOREIGN KEY (CALENDER_KEY)     
-    REFERENCES [Utility].[Calender] (CALENDER_KEY); 
+    REFERENCES [dim].[Calender] (CALENDER_KEY); 
