f5b4f08794cd2c5cc7f9621f67ee4286581160f8
unknown <melina.hartley@redlab.io>
2018-10-02 18:11:24 +0200

    changes

---
+++ Star/Tables/Measures/Nelson_Rules.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS [measure].[Nelson Rules];
+
+CREATE TABLE [measure].[Nelson Rules]
+(
+    [NELSON_RULES_KEY]	   				            	BIGINT IDENTITY(1,1)	 NOT NULL,
+	[Nelson Rule 1]	           							DECIMAL(19,2)	    	 NOT NULL,
+	[Nelson Rule 2]	           							DECIMAL(19,2)	    	 NOT NULL,
+    [Nelson Rule 3]	           							DECIMAL(19,2)	    	 NOT NULL,
+    [Nelson Rule 4]	           							DECIMAL(19,2)	    	 NOT NULL,
+    [Nelson Rule 5]	           							DECIMAL(19,2)	    	 NOT NULL,
+    [Nelson Rule 6]	           							DECIMAL(19,2)	    	 NOT NULL,
+    [Nelson Rule 7]	           							DECIMAL(19,2)	    	 NOT NULL,
+    [Nelson Rule 8]	           							DECIMAL(19,2)	    	 NOT NULL,
+    [Nelson Rule 9]	           							DECIMAL(19,2)	    	 NOT NULL,
+    [Nelson Rule 10]	           						DECIMAL(19,2)	    	 NOT NULL,
+    [Nelson Rule 11]	           						DECIMAL(19,2)	    	 NOT NULL,
+);
